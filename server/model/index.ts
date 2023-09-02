2d7b52f42619e741a78dff84dfb7e72373892c3e
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-09-02 14:02:25 -0700

    Finally connected server to db

--- server/model/index.ts
+++ server/model/index.ts
@@ -1,7 +1,32 @@
 const axios = require('axios');
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(process.env.MONGO_URI);
 
 module.exports = {
-  getWordsFromDB(callback: Function) {
-    console.log('i happened');
+  async getWordsFromDB(callback: Function) {
+
+    try {
+      await client.connect((err: Error | null) => {
+        if (err) {
+          console.error('Error connecting to MongoDB:', err);
+        } else {
+          console.log('Connected to MongoDB');
+        }
+      });
+
+
+      const db = client.db("bugginout");
+      const words = db.collection("words");
+
+      const data = await words.find({}).toArray();
+
+      console.log('data is', data);
+
+      callback(null, data);
+    } catch (error) {
+      console.error("Error querying the database:", error);
+      callback(error);
+    }
   }
 }
