e682c2783e11d820da496cbfc6385fa154c0856f
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-09-02 02:14:51 -0700

    Set up server

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,16 @@ const pathing = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const routing = require('./router');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(pathing.join(__dirname, '../client/dist')));
+app.use('/', routing);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server available at http://localhost${PORT}`);
 });
+
---
+++ server/model/index.ts
@@ -0,0 +1,7 @@
+const axios = require('axios');
+
+module.exports = {
+  getWordsFromDB(callback: Function) {
+    console.log('i happened');
+  }
+}
